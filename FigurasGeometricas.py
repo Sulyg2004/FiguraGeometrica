a9d45098ed93580cd23b702ecddbb688d133a9fe
Sulyg2004 <suly.grandac@ug.edu.ec>
2025-05-07 09:39:04 -0500

    FigurasGeometricas.py

--- FigurasGeometricas.py
+++ FigurasGeometricas.py
@@ -8,3 +8,16 @@ class Rectangulo:
 
     def perimetro(self):
         return 2 * (self.ancho + self.alto)
+
+class Cuadrado(Rectangulo):
+    def __init__(self, lado):
+        super().__init__(lado, lado)
+
+# Ejemplo de uso
+rectangulo = Rectangulo(8, 2)
+print("Área del rectángulo:", rectangulo.area())
+print("Perímetro del rectángulo:", rectangulo.perimetro())
+
+cuadrado = Cuadrado(10)
+print("Área del cuadrado:", cuadrado.area())
+print("Perímetro del cuadrado:", cuadrado.perimetro
